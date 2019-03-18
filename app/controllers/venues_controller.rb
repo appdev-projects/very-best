752287f8e75b2cbfed54d382fe6f5e63cbd620ae
Jiali Yu <jyu6@chicagobooth.edu>
2019-03-18 21:18:26 +0000

    Final

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,9 +1,11 @@
 class VenuesController < ApplicationController
   def index
-    @q = Venue.ransack(params.fetch("q", nil))
-    @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params.fetch("page", nil)).per(10)
+   @bookmarkedvenues=current_user.bookmarked_venues
+    
+    @q = @bookmarkedvenues.ransack(params.fetch("q", nil))
+    @bookmarkedvenues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params.fetch("page", nil)).per(10)
 
-    @location_hash = Gmaps4rails.build_markers(@venues.where.not(:address_latitude => nil)) do |venue, marker|
+    @location_hash = Gmaps4rails.build_markers(@bookmarkedvenues.where.not(:address_latitude => nil)) do |venue, marker|
       marker.lat venue.address_latitude
       marker.lng venue.address_longitude
       marker.infowindow "<h5><a href='/venues/#{venue.id}'>#{venue.created_at}</a></h5><small>#{venue.address_formatted_address}</small>"
